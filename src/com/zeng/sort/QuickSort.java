9fc4d16bb88922f09e6051cff15a84184c58e1e8
zengjj <jjz@appjia.com>
2021-05-14 16:36:05 +0800

    test

--- src/com/zeng/sort/QuickSort.java
+++ src/com/zeng/sort/QuickSort.java
@@ -20,7 +20,9 @@ public class QuickSort {
 			return;
 		}
 		
+		//先添加一些规划数组
 		int p = partition(arr, left, right);
+		//再对两部分数组分别做快速排序
 		quickSort(arr, left, p - 1);
 		quickSort(arr, p + 1, right);
 	}
